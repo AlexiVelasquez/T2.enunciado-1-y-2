376d917a858c9a9dfa289015c91c3b079e60b283
Fiorella Celeste Romero Reyes <N00389449@upn.pe>
2024-05-30 20:57:25 -0500

    Agregue texto

--- T2enunciado2/Form2.Designer.cs
+++ T2enunciado2/Form2.Designer.cs
@@ -28,23 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtAsc = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.Números = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtArray = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtAsc
             // 
-            this.textBox1.Location = new System.Drawing.Point(290, 128);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtAsc.Location = new System.Drawing.Point(290, 203);
+            this.txtAsc.Name = "txtAsc";
+            this.txtAsc.Size = new System.Drawing.Size(183, 20);
+            this.txtAsc.TabIndex = 0;
+            this.txtAsc.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // dataGridView1
             // 
@@ -56,6 +58,11 @@
             this.dataGridView1.Size = new System.Drawing.Size(148, 305);
             this.dataGridView1.TabIndex = 1;
             // 
+            // Números
+            // 
+            this.Números.HeaderText = "Números";
+            this.Números.Name = "Números";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -66,14 +73,9 @@
             this.label1.Text = "Insertar números:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Números
-            // 
-            this.Números.HeaderText = "Números";
-            this.Números.Name = "Números";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(301, 44);
+            this.button1.Location = new System.Drawing.Point(302, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 62);
             this.button1.TabIndex = 3;
@@ -83,32 +85,51 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(301, 202);
+            this.button2.Location = new System.Drawing.Point(302, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(150, 62);
             this.button2.TabIndex = 4;
             this.button2.Text = "Ordenar de forma descendente";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtDesc
             // 
-            this.textBox2.Location = new System.Drawing.Point(290, 285);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(183, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtDesc.Location = new System.Drawing.Point(290, 330);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(183, 20);
+            this.txtDesc.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 54);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Lista de números";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(366, 49);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(183, 20);
+            this.txtArray.TabIndex = 7;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 378);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtDesc);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.txtAsc);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -120,13 +141,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtAsc;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Números;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtArray;
     }
 }
 
--- T2enunciado2/Form2.cs
+++ T2enunciado2/Form2.cs
@@ -32,10 +32,30 @@ namespace T2enunciado2
 
         }
 
-        //declaracion lista
+        
+
         private void button1_Click(object sender, EventArgs e)
         {
+            
 
+        }
+
+        //declaracion del numero
+        int[] numeroLista;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numeroLista = new int[dataGridView1.RowCount];
+
+            for (int i = 0; i < dataGridView1.RowCount - 1; i = i + 1)
+            {
+                numeroLista[i] = int.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+
+            for (int i = 0; i < numeroLista.Count() - 1; i = i + 1)
+            {
+                txtArray.Text = txtArray.Text + ", " + numeroLista[i].ToString();
+            }
         }
     }
 }
